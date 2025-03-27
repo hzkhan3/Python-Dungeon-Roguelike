ad2ea2d4d14c5b1af51c68870d8b10b1d4073b2b
Haroon Khan <hzkhan3@yahoo.com>
2025-03-26 19:40:09 -0700

    Created Dungeon class + extracting tiles into dict

--- Code/dungeon.py
+++ Code/dungeon.py
@@ -2,4 +2,18 @@ import pygame
 import os 
 
 class Dungeon:
-    def __init__(self, x, y, )
+    def __init__(self, width, height, tileset_image, tile_size = 16):
+        self.width = width
+        self.height = height
+        self.tile_size = tile_size
+        self.tiles = self.load_tiles(tileset_image)
+    
+    def load_tiles(self, tileset_image):
+        tiles = {
+            'wall': {
+                'top': [tileset_image.subsurface((16, 0, self.tile_size, self.tile_size)),
+                        tileset_image.subsurface((32, 0, self.tile_size, self.tile_size)),
+                        tileset_image.subsurface((48, 0, self.tile_size, self.tile_size))],
+                'bottom': [tileset_image.subsurface((16, 0, self.tile_size, self.tile_size))]
+            }
+        }
